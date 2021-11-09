08a1f1227e19e06a8bb6ad15ed7e9815bd7b5f7b
jack-ie <jackie919@outlook.com>
2021-11-08 20:49:49 -0600

    updated api link modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,31 +12,30 @@
 
 
 
-// covid data
-
+// covid data functions, variables, and appended data
 //insert covid data at the different levels in dates specified. 
-    //if date is not specified, then populate the soonest upcoming
+
+// declare variables for covidData function
 var covidAPIKey = '7e0f83a022e24a68ae76e69913b1283c'
 var urlLink = 'https://api.covidactnow.org/v2/state/{state}.json?apiKey='
-var cityInput = ''
-// https://api.covidactnow.org/v2/state/{state}.json?apiKey=YOUR_KEY_HERE
-// https://api.covidactnow.org/v2/county/{fips}.json?apiKey=YOUR_KEY_HERE
-// https://api.covidactnow.org/v2/cbsa/{cbsa_code}.json?apiKey=YOUR_KEY_HERE
-
-function covidData (state){
-    var queryURL = urlLink + cityInput + covidAPIKey
+var state = ''
+// function to bring back covid data for each state
+function covidData (){
+    var queryURL = 'https://api.covidactnow.org/v2/states.timeseries.json?apiKey=7e0f83a022e24a68ae76e69913b1283c'
     fetch (queryURL)
         .then(function(response){
-            return response.json()
-        .then(function(response){
-            console.log(response);
-            for (var i = 0; i<data.length; i++){
+            return response.json()})
+        .then(function(covidDat){
+            console.log(covidDat);  /*console log data string*/
+
+        for (var i = 0; i < data.length; i++){
                 var state = document.createElement('p');
                 state.textContent = data[i].state;
-            }
-        })
+        }
     })
-    
+}
+covidData();
+
     //US on landing page
     //searched state
         //if the city is null, then provide state data
