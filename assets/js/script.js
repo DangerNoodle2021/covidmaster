46468a2b59ad3bb98b34c41e317a20bfbc93a4e8
jack-ie <jackie919@outlook.com>
2021-11-10 20:42:39 -0600

    added tags and updated ajax 	new file:   assets/images/covidimages/AK.jpg 	new file:   assets/images/covidimages/AL.jpg 	new file:   assets/images/covidimages/AR.jpg 	new file:   assets/images/covidimages/AZ.jpg 	new file:   assets/images/covidimages/CA.jpg 	new file:   assets/images/covidimages/CO.jpg 	new file:   assets/images/covidimages/CT.jpg 	new file:   assets/images/covidimages/DE.jpg 	new file:   assets/images/covidimages/FL.jpg 	new file:   assets/images/covidimages/GA.jpg 	new file:   assets/images/covidimages/HI.jpg 	new file:   assets/images/covidimages/IA.jpg 	new file:   assets/images/covidimages/ID.jpg 	new file:   assets/images/covidimages/IL.jpg 	new file:   assets/images/covidimages/IN.jpg 	new file:   assets/images/covidimages/KS.jpg 	new file:   assets/images/covidimages/KY.jpg 	new file:   assets/images/covidimages/LA.jpg 	new file:   assets/images/covidimages/MA.jpg 	new file:   assets/images/covidimages/MD.jpg 	new file:   assets/images/covidimages/ME.jpg 	new file:   assets/images/covidimages/MI.jpg 	new file:   assets/images/covidimages/MN.jpg 	new file:   assets/images/covidimages/MO.jpg 	new file:   assets/images/covidimages/MS.jpg 	new file:   assets/images/covidimages/MT.jpg 	new file:   assets/images/covidimages/NC.jpg 	new file:   assets/images/covidimages/ND.jpg 	new file:   assets/images/covidimages/NE.jpg 	new file:   assets/images/covidimages/NH.jpg 	new file:   assets/images/covidimages/NJ.jpg 	new file:   assets/images/covidimages/NM.jpg 	new file:   assets/images/covidimages/NV.jpg 	new file:   assets/images/covidimages/NY.jpg 	new file:   assets/images/covidimages/OH.jpg 	new file:   assets/images/covidimages/OK.jpg 	new file:   assets/images/covidimages/OR.jpg 	new file:   assets/images/covidimages/PA.jpg 	new file:   assets/images/covidimages/RI.jpg 	new file:   assets/images/covidimages/SC.jpg 	new file:   assets/images/covidimages/SD.jpg 	new file:   assets/images/covidimages/TN.jpg 	new file:   assets/images/covidimages/TX.jpg 	new file:   assets/images/covidimages/UT.jpg 	new file:   assets/images/covidimages/VA.jpg 	new file:   assets/images/covidimages/VT.jpg 	new file:   assets/images/covidimages/WA.jpg 	new file:   assets/images/covidimages/WI.jpg 	new file:   assets/images/covidimages/WV.jpg 	new file:   assets/images/covidimages/WY.jpg 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,79 @@
-//Defining a baseURL and key to as part of the request URL
-const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=30&city=chicago&startDateTime=2021-11-21T00:00:00Z&endDateTime=2021-11-25T00:00:00Z&apikey=';
-const KEY = 'wYk8muWgRYtJ2K4cZHocmXVoL8gum9hh'; 
+// //Defining a baseURL and key to as part of the request URL
+// const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=30&city=chicago&startDateTime=2021-11-21T00:00:00Z&endDateTime=2021-11-25T00:00:00Z&apikey=';
+// const KEY = 'wYk8muWgRYtJ2K4cZHocmXVoL8gum9hh'; 
 
-  // Grab references to all the DOM elements you'll need to manipulate
+//   // Grab references to all the DOM elements you'll need to manipulate
 
-  const searchTerm = document.querySelector('.search');
-  const startDate = document.querySelector('.start-date');
-  const endDate = document.querySelector('.end-date');
-  const searchForm = document.querySelector('form');
+//   const searchTerm = document.querySelector('.search');
+//   const startDate = document.querySelector('.start-date');
+//   const endDate = document.querySelector('.end-date');
+//   const searchForm = document.querySelector('form');
 
 
-    // Event listeners to control the functionality
-searchForm.addEventListener('submit', submitSearch);
+//     // Event listeners to control the functionality
+// searchForm.addEventListener('submit', submitSearch);
 
-function submitSearch(e) {
-    pageNumber = 0;
-    fetchResults(e);
-  }
+// function submitSearch(e) {
+//     pageNumber = 0;
+//     fetchResults(e);
+//   }
   
-  function fetchResults(e) {
+//   function fetchResults(e) {
   
-    // Assemble the full URL
-    url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")';
-  }
-    if(startDate.value !== '11-11-2021') {
-      url += '&begin_date=' + startDate.value;
-    };
+//     // Assemble the full URL
+//     url = baseURL + '?api-key=' + key + '&page=' + pageNumber + '&q=' + searchTerm.value + '&fq=document_type:("article")';
+//   }
+//     if(startDate.value !== '11-11-2021') {
+//       url += '&begin_date=' + startDate.value;
+//     };
   
-    if(endDate.value !== '02-02-2022') {
-      url += '&end_date=' + endDate.value;
-    };
+//     if(endDate.value !== '02-02-2022') {
+//       url += '&end_date=' + endDate.value;
+//     };
     
-    // Use fetch() to make the request to the API
-fetch(url).then(function(result) {
-    return result.json();
-  }).then(function(json) {
-    displayResults(json);
-  });
+//     // Use fetch() to make the request to the API
+// fetch(url).then(function(result) {
+//     return result.json();
+//   }).then(function(json) {
+//     displayResults(json);
+//   });
   
-  function displayResults(json) {
-    while (section.firstChild) {
-        section.removeChild(section.firstChild);
-    }
-  }
-    const events = json.response.docs;
+//   function displayResults(json) {
+//     while (section.firstChild) {
+//         section.removeChild(section.firstChild);
+//     }
+//   }
+//     const events = json.response.docs;
   
-    if(events.length === 20) {
-      nav.style.display = 'block';
-    } else {
-      nav.style.display = 'none';
-    }
+//     if(events.length === 20) {
+//       nav.style.display = 'block';
+//     } else {
+//       nav.style.display = 'none';
+//     }
 
 
-//the forms for the search fields save to local storage
-let stateSearch = "IL";
+// //the forms for the search fields save to local storage
+// let stateSearch = "IL";
 
-function fetchObjects() {
-    const url = BASE_URL+KEY+"&stateCode=" + stateSearch
+// function fetchObjects() {
+//     const url = BASE_URL+KEY+"&stateCode=" + stateSearch
    
     
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (ticketmasterData) {
-            console.log(ticketmasterData._embedded)
+//     fetch(url)
+//         .then(function (response) {
+//             return response.json()
+//         })
+//         .then(function (ticketmasterData) {
+//             console.log(ticketmasterData._embedded)
             
-        })
+//         })
 
-        .catch(function (error) {
-            console.error(error);
-        });
-}
+//         .catch(function (error) {
+//             console.error(error);
+//         });
+// }
 
-fetchObjects();
+// fetchObjects();
 //use the values in the form fields to apply filters to the api data 
 
 
@@ -91,44 +91,39 @@ fetchObjects();
 
 // declare variables for covidData function
 var covidAPIKey = '8530c235e49149a59a1313f66d8492a8'
-var state = document.getElementById('state')
+var state = document.querySelector('#state')
 var urlLink = `https://api.covidactnow.org/v2/states/${state}.timeseries.json?apiKey=`
 var fetchBtn = document.getElementById('search')
 
 function covidData(state){
     var queryURL = urlLink + covidAPIKey;
     // AJAX call for covid data
-    $.ajax({
+    $.ajax ({
         url: queryURL,
-        method: 'GET'
+        method: 'GET',
     }).then(function(response){
-        var currData = $("<div id = 'currentData'>");
+        var currData = $('#currentData');
 
         var titleSt = response.state;
-        titleSt.append(currData);
+        var h3 = $('<h3>').text(titleSt);
+        currData.append(h3);
 
         var riskLv = response.riskLevels.overall;
-        riskLv.append(currData);
+        var p1 = $('<p>').text(riskLv);
+        currData.append(p1);
 
         var popVax = response.metrics.vaccinationsCompletedRatio;
-        popVax.append(currData);
+        var p2 = $('<p>').text(popVax);
+        currData.append(p2);
 
         var newCases = response.actuals.newCases;
-        newCases.append(currData);
+        var p3 = $('<p>').text(newCases);
+        currData.append(p3);
 
         var posTests = response.actuals.positiveTests;
-        posTests.append(currData);
-
-        var infoUrl = response.url;
-        infoUrl.append(currData);
+        var p4 = $('<p>').text(posTests);
+        currData.append(p4);      
     })
-}
-
-function getcovidData(state){
-    if(typeof state === 'object'){
-        state = $(this).attr('data-name');
-    }
-    covidData(state);
 }
 
 // function to bring back covid data for each state
@@ -147,6 +142,7 @@ function getcovidData(state){
 //     })
 // }
 covidData();
+
 // gets covid data upon clicking the search button
 // fetchBtn.addEventListener('click', covidData);
 
